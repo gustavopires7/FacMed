2c59a106a1279e00057854202325a9d0bfddb2b4
Nadja001 <nadjacibelyfreitas@gmail.com>
2024-12-10 22:22:59 -0300

    cria as views necessárias no projeto

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,31 +1,70 @@
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse_lazy
+from .forms import UsuarioCreationForm, UsuarioUpdateForm
+from .models import Usuario
 
+class IndexView(TemplateView):
+    template_name = 'usuarios/index.html'
 
+class UserRegisterView(CreateView):
+    model = Usuario
+    form_class = UsuarioCreationForm
+    template_name = 'usuarios/form.html'
+    success_url = reverse_lazy('login')
 
-def index(request):
-    return render(request, 'usuarios/index.html')
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(self.request, user)
+        return response
 
+class UserLoginView(TemplateView):
+    template_name = 'usuarios/login.html'
 
-def profile(request):
-    return render(request, 'usuarios/profile.html')
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, self.template_name, {'error': 'Credenciais inválidas'})
 
+class UserLogoutView(TemplateView):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
 
-def user_register(request):
-    return render(request, 'usuarios/form.html')
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'usuarios/profile.html'
+    login_url = 'login'
 
-def user_login(request):
-    return render(request, 'usuarios/login.html')
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    model = Usuario
+    form_class = UsuarioUpdateForm
+    template_name = 'usuarios/form.html'
+    success_url = reverse_lazy('profile')
+    login_url = 'login'
 
-def user_logout(request):
-    logout(request)
-    return redirect('login')
+    def get_object(self, queryset=None):
+        return self.request.user
 
+class ProfileDeleteView(LoginRequiredMixin, DeleteView):
+    model = Usuario
+    template_name = 'usuarios/confirm_delete.html'
+    success_url = reverse_lazy('login')
+    login_url = 'login'
 
-def edit_profile(request):
-    return render(request, 'usuarios/form.html')
+    def get_object(self, queryset=None):
+        return self.request.user
 
-
-def delete_profile(request):
-    # delete user
-    return redirect('login')
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return super().delete(request, *args, **kwargs)
